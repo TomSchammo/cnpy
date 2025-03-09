bee8522abea2f50b5d4aff94e59c0be010ff7353
Tom Schammo <tom@tomschammo.com>
2025-03-09 01:59:25 +0100

    refactor: Use C++ concepts to map types

--- include/cnpy/cnpy.hpp
+++ include/cnpy/cnpy.hpp
@@ -87,37 +87,39 @@ consteval char get_endianness() {
     return '>';
 }
 
+template <typename T>
+concept FloatingPoint = std::is_same_v<T, float> || std::is_same_v<T, double> ||
+                        std::is_same_v<T, long double>;
+
+template <typename T>
+concept SignedInteger = std::is_same_v<T, int> || std::is_same_v<T, char> ||
+                        std::is_same_v<T, short> || std::is_same_v<T, long> ||
+                        std::is_same_v<T, long long>;
+
+template <typename T>
+concept UnsignedInteger =
+    std::is_same_v<T, unsigned char> || std::is_same_v<T, unsigned short> ||
+    std::is_same_v<T, unsigned int> || std::is_same_v<T, unsigned long> ||
+    std::is_same_v<T, unsigned long long>;
+
+template <typename T>
+concept ComplexNumber = std::is_same_v<T, std::complex<float>> ||
+                        std::is_same_v<T, std::complex<double>> ||
+                        std::is_same_v<T, std::complex<long double>>;
+
 template <typename T> consteval char map_type() {
-  if constexpr (std::is_same_v<T, float> || std::is_same_v<T, double> ||
-                std::is_same_v<T, long double>) {
+  if constexpr (FloatingPoint<T>)
     return 'f';
-  }
-
-  if constexpr (std::is_same_v<T, int> || std::is_same_v<T, char> ||
-                std::is_same_v<T, short> || std::is_same_v<T, long> ||
-                std::is_same_v<T, long long>) {
+  else if constexpr (SignedInteger<T>)
     return 'i';
-  }
-
-  if constexpr (std::is_same_v<T, unsigned char> ||
-                std::is_same_v<T, unsigned short> ||
-                std::is_same_v<T, unsigned long> ||
-                std::is_same_v<T, unsigned long long> ||
-                std::is_same_v<T, unsigned int>) {
+  else if constexpr (UnsignedInteger<T>)
     return 'u';
-  }
-
-  if constexpr (std::is_same_v<T, bool>) {
+  else if constexpr (std::is_same_v<T, bool>)
     return 'b';
-  }
-
-  if constexpr (std::is_same_v<T, std::complex<float>> ||
-                std::is_same_v<T, std::complex<double>> ||
-                std::is_same_v<T, std::complex<long double>>) {
+  else if constexpr (ComplexNumber<T>)
     return 'c';
-  }
-
-  return '?';
+  else
+    return '?';
 }
 
 template <typename T>
